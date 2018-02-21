1670f60dc1af601620afa8afb7c29111042450c9
Hi <hi@His-MacBook-Pro.home>
2018-02-21 17:14:00 -0500

    mach 1

--- d05/ex20/ft_putnbr_base.c
+++ d05/ex20/ft_putnbr_base.c
@@ -1,12 +1,32 @@
-#include <stdio.h>
-char    const octal[9] = {'p','o','n','e','y','v','i','f'};
+#include <unistd.h>
+char    const octals[9] = {'0','1','2','3','4','5','6','7'};
 char    const binary[3] = {'0','1'};
 char    const base10[11] = {'0','1','2','3','4','5','6','7','8','9'};
 char    const base16[17] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-int     ft_lsearch(char key, char *base, int size)
+
+void    ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+void     ft_convert_base(int nbr, int base)
+{
+
+    while (nbr > 0)
+    {
+        ft_putchar(base16[nbr % base]);
+        nbr /= base;
+    }
+}
+
+int     ft_lsearch(char key, char const *base, int size)
 {
     int     i;
 
+    if (size == 16)
+    {
+        if ((key >= 'a') && (key <= 'z'))
+            key -= 32;
+    }
     i = 0;
     while (i < size)
     {
@@ -19,12 +39,12 @@ int     ft_lsearch(char key, char *base, int size)
 int     ft_is_base(char *base, int size)
 {
     int     i;
-    char    *basesys;
+    char    const *basesys;
 
     if (size == 2)
         basesys = binary;
     else if (size == 8)
-        basesys = octal;
+        basesys = octals;
     else if (size == 10)
         basesys = base10;
     else if (size == 16)
@@ -59,13 +79,13 @@ void    ft_putnbr_base(int nbr, char *base)
     len = ft_strlen(base);
     if (ft_is_base(base, len))
     {
-        printf("it works: base:%s\nnbr:%d\n", base, nbr);
+        ft_convert_base(nbr, len);
     }
 }
 int     main()
 {
-    char    *base = "poneyvif";
-    ft_putnbr_base(44, octal);
+    char    base[17] = {'0','1','2','3','4','5','6','7','8','9','a','B','C','d','E','F'};
+    ft_putnbr_base(282, base);
 
     return 0;
 }
