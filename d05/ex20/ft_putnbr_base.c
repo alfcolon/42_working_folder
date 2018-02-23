fcb9d7d790c1b13fed68985076137689c0905da6
Hi <hi@His-MacBook-Pro.home>
2018-02-23 07:36:19 -0500

    fixed bad merge issue

--- d05/ex20/ft_putnbr_base.c
+++ d05/ex20/ft_putnbr_base.c
@@ -1,35 +1,17 @@
-<<<<<<< HEAD
+
 int     i;
 int     j;
 
 int	ft_iterative_power(int nb, int power)
-=======
-#include <stdio.h>
-char    const octals[9] = {'0','1','2','3','4','5','6','7'};
-char    const binary[3] = {'0','1'};
-char    const base10[11] = {'0','1','2','3','4','5','6','7','8','9'};
-char    const base16[17] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-int     ft_lsearch(char key, char const *base, int size)
->>>>>>> 8df9da15f355336599594be8553714e55256e977
 {
     int		total;
 
-<<<<<<< HEAD
     if (nb <= 0)
         return (0);
     else if (power == 0)
         return (1);
     total = 1;
     while (power > 0)
-=======
-    if (size == 16)
-    {
-        if ((key >= 'a') && (key <= 'z'))
-            key -= 32;
-    }
-    i = 0;
-    while (i < size)
->>>>>>> 8df9da15f355336599594be8553714e55256e977
     {
         total *= nb;
         power--;
@@ -38,7 +20,6 @@ int     ft_lsearch(char key, char const *base, int size)
 }
 int     ft_base10(char *str, int base, int size)
 {
-<<<<<<< HEAD
     int 	total;
     int 	sign;
     int 	power;
@@ -85,21 +66,6 @@ int	    ft_get_atoi_base(char *str)
     int     base;
     int     base_n;
     int     k;
-=======
-    int     i;
-    char    const *basesys;
-
-    if (size == 2)
-        basesys = binary;
-    else if (size == 8)
-        basesys = octals;
-    else if (size == 10)
-        basesys = base10;
-    else if (size == 16)
-        basesys = base16;
-    else
-        return (0);
->>>>>>> 8df9da15f355336599594be8553714e55256e977
 
     base = 0;
     k = 0;
@@ -175,7 +141,6 @@ int     ft_dupes(char *str)
 }
 int	ft_atoi_base10(char *dest, char *src, int str_base)
 {
-<<<<<<< HEAD
     int     start;
     int     k;
     int     atoi;
@@ -211,10 +176,4 @@ int     ft_atoi_base(char *str, char *base)
         str_base = base_base;
     str_atoi = ft_atoi_base10(temp, str, str_base);
     return (str_atoi);
-=======
-    char    base[17] = {'0','1','2','3','4','5','6','7','8','9','a','B','C','d','E','F'};
-    ft_putnbr_base(44, base);
-
-    return 0;
->>>>>>> 8df9da15f355336599594be8553714e55256e977
 }
